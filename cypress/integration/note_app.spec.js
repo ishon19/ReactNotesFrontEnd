f615a64ad2de84db30b18c50edcd30f01ac35e48
ishon19 <pathak.shreyans@gmail.com>
2020-12-07 07:23:00 +0530

    more tests

--- cypress/integration/note_app.spec.js
+++ cypress/integration/note_app.spec.js
@@ -23,20 +23,25 @@ describe("Note App", () => {
     cy.contains("Shashwat Pathak Logged In");
   });
 
-  it.only("login fails with wrong password", function () {
+  it("login fails with wrong password", function () {
     cy.contains("Login").click();
     cy.get("#username").type("shashwat");
     cy.get("#password").type("wrongPassword");
     cy.get("#login-button").click();
-    cy.contains("Wrong Credentials");
+    cy.get(".error")
+      .should("contain", "Wrong Credentials")
+      .and("have.css", "color", "rgb(255, 0, 0)")
+      .and("have.css", "border-style", "solid");
+    cy.get("html").should("not.contain", "Shashwat Pathak Logged In");
   });
 
   describe("when logged in", function () {
     beforeEach(function () {
-      cy.contains("Login").click();
+      /* cy.contains("Login").click();
       cy.get("input:first").type("shashwat");
       cy.get("input:last").type("myPassword");
-      cy.get("#login-button").click();
+      cy.get("#login-button").click(); */
+      cy.login({ username: "shashwat", password: "myPassword" });
     });
 
     it("a new note can be created", function () {
@@ -48,14 +53,32 @@ describe("Note App", () => {
 
     describe("and a note exists", function () {
       beforeEach(function () {
-        cy.get("#new-note-button").click();
+        /* cy.get("#new-note-button").click();
         cy.get("#newNote").type("another note cypress");
-        cy.contains("Save Note").click();
+        cy.contains("Save Note").click(); */
+        cy.createNote({
+          content: "another note by cypress",
+          important: false,
+        });
       });
 
       it("it can be made important", function () {
-        cy.contains("another note cypress").contains("Make Important").click();
-        cy.contains("another note cypress").contains("Make not important");
+        cy.contains("another note by cypress")
+          .contains("Make Important")
+          .click();
+        cy.contains("another note by cypress").contains("Make not important");
+      });
+    });
+
+    describe("and several notes exist", function () {
+      beforeEach(function () {
+        cy.createNote({ content: "First Note", important: false });
+        cy.createNote({ content: "Second Note", important: false });
+        cy.createNote({ content: "Third Note", important: false });
+      });
+      it("one of those can be made important", function () {
+        cy.contains("Second Note").contains("Make Important").click();
+        cy.contains("Second Note").contains("Make not important");
       });
     });
   });
